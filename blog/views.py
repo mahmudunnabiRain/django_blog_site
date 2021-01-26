9daee9760c159ac290d06103900637bf44afbfb6
Mahmudunnabi Rain <mahmudunnabirain@gmail.com>
2021-01-26 16:45:26 +0600

    update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 
 contents =[
@@ -15,12 +16,22 @@ contents =[
     'text': 'Second blog in our project'}
 ]
 
-def home(request):
-    context = {
-        'posts': Post.objects.all(), 
-    }
-    return render(request, 'home.html', context)
-
 def about(request):
     #return HttpResponse('<h1>About</h1>')
     return render(request, 'about.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'post_form.html'
+
--- django_site/settings.py
+++ django_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
